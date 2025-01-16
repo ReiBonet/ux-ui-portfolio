81cc4ffdaf87a728a2d8ff73136f35ebba402e24
streihermit <reinaldobonet@gmail.com>
2025-01-16 12:12:25 -0500

    Changes to add a hamburger menu and transition to make it slide from the right to the left of the screen

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Selectors
+const navToggle = document.querySelector(".nav-toggle"); // Hamburger
+const overlayMenu = document.getElementById("overlayMenu"); // Fullscreen overlay
+const closeMenuBtn = document.querySelector(".close-menu"); // The close (X) button
+
+// Open overlay on hamburger click
+navToggle.addEventListener("click", () => {
+overlayMenu.classList.add("open");
+});
+
+// Close overlay on "X" button click
+closeMenuBtn.addEventListener("click", () => {
+overlayMenu.classList.remove("open");
+});
+
+/* Optional: Close overlay when a link is clicked
+overlayMenu.querySelectorAll("a").forEach(link => {
+link.addEventListener("click", () => {
+    overlayMenu.classList.remove("open");
+});
+});
+*/
